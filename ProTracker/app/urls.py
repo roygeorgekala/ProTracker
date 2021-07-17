17f4db2f97f669ecdd8a39edf7c5b093fd0313a5
RoyGeorge29 <roygeorgekala@gmail.com>
2021-07-17 10:33:43 +0530

    Adding required functionality and removed the social media

--- ProTracker/app/urls.py
+++ ProTracker/app/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.redirection,name='redirect'),
-    path('login/', views.login, name='redirect'),
+    path('login/', views.login, name='login'),
+    path('login/verify', views.verify, name='verify'),
     path('tasks/<int:id>/testing', views.testing, name='test'),
     path('tasks/<int:id>/', views.taskView, name='tasks'),
 ]
--- ProTracker/app/views.py
+++ ProTracker/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Task
 # Create your views here.
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
 
 def taskView(request, id):
@@ -25,8 +26,18 @@ def testing(request, id):
 
 
 def login(request):
-    return render(request,'app/login.html')
+    return render(request, 'app/login.html')
 
 
 def redirection(request):
     return redirect('/login')
+
+
+def verify(request):
+
+    if request.method == 'GET':
+        return redirect('/login')
+    elif request.method == 'POST':
+        print(request.POST.get('username'), request.POST.get('password'))
+
+    return render(request, 'app/testing.html')
