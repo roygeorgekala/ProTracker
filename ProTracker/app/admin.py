cfd02b4007d6669f06a51af70d74638d0332497c
RoyGeorge29 <roygeorgekala@gmail.com>
2021-07-18 09:07:12 +0530

    Updating work on JS and Django backend, added all pages.

--- ProTracker/app/admin.py
+++ ProTracker/app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from.models import Employee, Task
+from.models import Employee, Task, Manager
 # Register your models here.
 
 
-
+admin.site.register(Manager)
 admin.site.register(Employee)
 admin.site.register(Task)
--- ProTracker/app/urls.py
+++ ProTracker/app/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.redirection,name='redirect'),
+    path('', views.redirection, name='redirect'),
     path('login/', views.login, name='login'),
     path('login/verify', views.verify, name='verify'),
-    path('tasks/<int:id>/testing', views.testing, name='test'),
-    path('tasks/<int:id>/', views.taskView, name='tasks'),
+    path('tasks/<str:username>/', views.taskView, name='tasks'),
+    path('tasks/<str:username>/change/<str:task_id>',
+         views.changeTask, name='changeTask'),
+    path('tasks/<str:username>/refresh/', views.refresh, name='refresh'),
+    path('tasks/<str:username>/logout', views.logout, name='logout'),
+    path('manager/<str:username>', views.manager, name='manager'),
 ]
