36451eae0022963184af4f2031b9a47cf0603817
vxshnuprxya <vishnupriyamohankumar11@gmail.com>
2021-07-25 19:58:18 +0530

    changes to login page

--- ProTracker/app/static/js/login.js
+++ ProTracker/app/static/js/login.js
@@ -1,12 +1,11 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
+const about_us_btn = document.querySelector("#about-us-btn");
 const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+about_us_btn.addEventListener("click", () => {
+  container.classList.add("about-us-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
-
+  container.classList.remove("about-us-mode");
+}); 
